b86871a74ee718f80617413cdc6c05e57749ce82
yiheng <1611743891@qq.com>
2023-06-03 02:23:21 +0800

    Initial commit

---
+++ spring-boot-autoconfigurer/src/main/java/com/example/springbootautoconfigurer/SpringBootAutoconfigurerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootautoconfigurer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAutoconfigurerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAutoconfigurerApplication.class, args);
+    }
+
+}
---
+++ spring-boot-autoconfigurer/src/test/java/com/example/springbootautoconfigurer/SpringBootAutoconfigurerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootautoconfigurer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAutoconfigurerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
